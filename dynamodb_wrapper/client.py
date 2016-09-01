5b11810337309235c0f185e8bffba272dce756b0
Vicente Neto <vicente.silva@embedded.ufcg.edu.br>
2016-09-01 00:20:59 -0300

    Created service to save itens

--- dynamodb_wrapper/client.py
+++ dynamodb_wrapper/client.py
@@ -1,9 +1,15 @@
+import uuid
+
 from dynamodb_wrapper.meta import ModelMetadata
 
 
 class Client(object):
     def __init__(self, connection):
         self.connection = connection
+
+    @staticmethod
+    def __generate_uuid():
+        return str(uuid.uuid1()).replace('-', '')
 
     def create_schema(self, model, provisioned_throughput=None, wait=False):
         meta = ModelMetadata(model)
@@ -15,3 +21,16 @@ class Client(object):
             }
 
         meta.create_table(self.connection, provisioned_throughput, wait)
+
+    def delete_schema(self, model, wait=False):
+        meta = ModelMetadata(model)
+
+        meta.delete_table(self.connection, wait)
+
+    def save(self, item):
+        meta = ModelMetadata(type(item))
+
+        if meta.hash_key.auto_generated:
+            setattr(item, meta.hash_key.name, self.__generate_uuid())
+
+        self.connection.Table(meta.name).put_item(Item=item.to_json())
--- dynamodb_wrapper/fields.py
+++ dynamodb_wrapper/fields.py
@@ -1,12 +1,22 @@
+class FieldType:
+    STRING = 'S'
+    NUMBER = 'N'
+    BINARY = 'B'
+
+
 class Field(object):
-    def __init__(self, type_='S', hash_key=False, range_key=False, name=None):
+    def __init__(self, type_=FieldType.STRING, hash_key=False, range_key=False, auto_generated=False, name=None):
         if hash_key and range_key:
             raise ValueError('hash_key and range_key are mutually exclusive!')
 
-        if all(attribute_type != type_ for attribute_type in ['S', 'N', 'B']):
+        if all(attribute_type != type_ for attribute_type in [FieldType.STRING, FieldType.NUMBER, FieldType.BINARY]):
             raise ValueError('Type must be one of: "S", "N" or "B"!')
+
+        if type_ != 'S' and auto_generated:
+            raise ValueError('Only string fields can be auto generated!')
 
         self.type = type_
         self.hash_key = hash_key
         self.range_key = range_key
+        self.auto_generated = auto_generated
         self.name = name
